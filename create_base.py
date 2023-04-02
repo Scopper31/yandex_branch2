a66cf57a60f401e9ae7e1f884fa7509301b0d890
Scopper31 <84668306+Scopper31@users.noreply.github.com>
2023-04-02 20:06:10 +0300

    Create create_base.py

---
+++ create_base.py
@@ -0,0 +1,26 @@
+import sqlite3
+from sqlite3 import Error
+from datetime import date, datetime, timedelta
+
+
+def sql_connection():
+    try:
+        con = sqlite3.connect('sqlite_python.db')
+        return con
+    except Error:
+        print(Error)
+
+
+def sql_table(con):
+    sqlite_create_table_query = '''CREATE TABLE sqlitedb_subscribers (
+                                        email TEXT NOT NULL UNIQUE,
+                                        ending_date timestamp);'''
+    cursor = con.cursor()
+    cursor.execute(sqlite_create_table_query)
+    con.commit()
+    cursor.close()
+
+
+sqlite_connection = sql_connection()
+sql_table(sqlite_connection)
+sqlite_connection.close()
